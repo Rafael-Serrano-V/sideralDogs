8eda22d273943fc29910214f6424de9e3902f5ee
WardMore <rafaelmserranov@gmail.com>
2024-03-22 15:57:06 -0300

    se aplica sugar syntax y comentarios mas explicativos

--- functions.js
+++ functions.js
@@ -1,45 +1,55 @@
-//Función para obtener las razas y sub-razas de perros
+//Función para obtener las razas y sub-razas de perros de la API
 const getDogBreeds = async () => {
-    const response = await fetch("https://dog.ceo/api/breeds/list/all");
-    const data = await response.json();
-    return data.message;
+    try {
+        const response = await fetch("https://dog.ceo/api/breeds/list/all");
+        const data = await response.json();
+        console.log(data.message)
+        // data.message contiene un objeto con las razas como claves y las sub-razas como valores
+        return data.message;
+    } catch (error) {
+        console.error("Error al obtener las razas de perros:", error);
+    }
 };
 
-//Función para generar el HTML de lista para las razas  y sub-razas de los perros
-const generateHTML = (breeds) => {
-    let html = "";
+//Función para generar el HTML para una sola raza
+const generateBreedHTML = (breed, subBreeds) => {
+    let breedHTML = `
+    <div class="d-flex w-100 list-group-item list-group-item-action list-group-item-success d-flex py-3">
+        <div>
+        <a href="#" class="breed text-decoration-none text-success" data-breed="${breed}" aria-current="true" data-bs-toggle="modal" data-bs-target="#dogModal"><span class="mb-2 fw-bold" style="text-transform: uppercase;">${breed}</span></a>`;
+    
+    if (subBreeds.length > 0) {
+        breedHTML += `<hr><strong>Sub-razas:</strong>`;
+        for (let subBreed of subBreeds) {
+            breedHTML += `<a href="#" class="subBreed text-decoration-none text-success" data-breed="${breed}" data-subBreed="${subBreed}" data-bs-toggle="modal" data-bs-target="#dogModal"><p style= "text-align: left;">• ${subBreed}</p></a>`;
+        }
+    }
+    breedHTML += `</div></div>`;
+    return breedHTML;
+};
+
+//Función para generar el HTML de lista para todas las razas y sub-razas de los perros
+const generateAllBreedsHTML = (breeds) => {
+    let allBreedsHTML = "";
     let breedCount = 0;
     let subBreedCount = 0;
 
     //Iteramos sobre cada raza
     for (let breed in breeds) {
-        html += `
-        <div class="d-flex w-100 list-group-item list-group-item-action list-group-item-success d-flex py-3">
-            <div>
-            <a href="#" class="breed text-decoration-none text-success" data-breed="${breed}" aria-current="true" data-bs-toggle="modal" data-bs-target="#dogModal"><span class="mb-2 fw-bold" style="text-transform: uppercase;">${breed}</span></a>`;
-        let subBreeds = breeds[breed];
-
-        //Si la raza tiene sub-razas, iterar sobre ellas
-        if (subBreeds.length > 0) {
-            html += `<hr><strong>Sub-razas:</strong>`;
-            for (let subBreed of subBreeds) {
-                html += `<a href="#" class="subBreed text-decoration-none text-success" data-breed="${breed}" data-subBreed="${subBreed}" data-bs-toggle="modal" data-bs-target="#dogModal"><p style= "text-align: left;">• ${subBreed}</p>`;
-                subBreedCount++;
-            }
-        }
-        html += `</div></div>`;
+        allBreedsHTML += generateBreedHTML(breed, breeds[breed]);
         breedCount++;
+        subBreedCount += breeds[breed].length;
     }
-    return { html, breedCount, subBreedCount };
+    return { html: allBreedsHTML, breedCount, subBreedCount };
 };
 
 //Función principal
 const fetchDogBreedsAll = async () => {
     const breeds = await getDogBreeds();
-    const { html, breedCount, subBreedCount } = generateHTML(breeds);
+    const { html: allBreedsHTML, breedCount, subBreedCount } = generateAllBreedsHTML(breeds);
 
     //Mostramos los resultados en la vista HTML
-    document.getElementById("dogBreeds").innerHTML = html;
+    document.getElementById("dogBreeds").innerHTML = allBreedsHTML;
     document.getElementById(
         "breedCount"
     ).innerHTML = `Total de razas: <span class="badge rounded-pill text-bg-dark">${breedCount}</span>`;
@@ -59,16 +69,16 @@ const fetchDogBreedsAll = async () => {
             let subBreed = target.dataset.subbreed;
             let url = `https://dog.ceo/api/breed/${breed}${subBreed ? `/${subBreed}` : ""
                 }/images/random`;
-
-            const response = await fetch(url);
-            const data = await response.json();
-
-            document.getElementById("dogImage").src = data.message;
+            
+            try {
+                const response = await fetch(url);
+                const data = await response.json();
+                document.getElementById("dogImage").src = data.message;
+            } catch (error) {
+                console.error("Error al obtener la imagen de la raza de perro:", error);
+            }
         });
     });
 };
 
 fetchDogBreedsAll();
-
-
-
